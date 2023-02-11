fc701b6e0bd240ad044d3f8d4ee3e701355b891b
Marc026 <mlapira@horizon.csueastbay.edu>
2023-02-10 22:57:20 -0800

    LoadFactor and started on Append.

--- ChunkList.cpp
+++ ChunkList.cpp
@@ -5,3 +5,90 @@
 #include "ChunkList.h"
 
 
+template<class T>
+ChunkList<T>::ChunkList() {
+    listLen = 0;
+    head = tail = nullptr;
+    iterNode;
+    arrPos = 0;
+
+    //for(int i = 0; i < ARRAY_SIZE; i++ ){
+    //    values[i] = -1
+    //}
+}
+
+template<class T>
+ChunkList<T>::ChunkList(T *arr, int arrLen) {
+
+}
+
+template<class T>
+ChunkList<T>::~ChunkList() {
+
+}
+
+template<class T>
+void ChunkList<T>::Append(T value) {
+    Node *insertNode = new Node;
+    if (head = nullptr) {
+        head = tail = new Node;
+    }
+    else if(!IsFull(insertNode->)){
+
+    }
+    else  {
+        tail->next = new Node;
+        tail = new Node;
+        insertNode->value[0];
+    }
+
+}
+
+
+
+template<class T>
+void ChunkList<T>::Remove(T value) {
+
+}
+
+template<class T>
+int ChunkList<T>::GetLength() {
+    return listLen;
+}
+
+template<class T>
+double ChunkList<T>::LoadFactor() {
+    double percent = 0;
+    if(listLen > 0){
+        percent = listLen / numChunks * ARRAY_SIZE;
+        return percent;
+    }
+    else
+    return IsEmpty();
+}
+
+template<class T>
+bool ChunkList<T>::Contains(T value) {
+
+    return false;
+}
+
+template<class T>
+T ChunkList<T>::GetIndex(int i) {
+    return nullptr;
+}
+
+template<class T>
+void ChunkList<T>::ResetIterator() {
+    arrPos = 0;
+}
+
+template<class T>
+T ChunkList<T>::GetNextItem() {
+    return nullptr;
+}
+
+template<class T>
+bool ChunkList<T>::IsEmpty() {
+    return true;
+}
